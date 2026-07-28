605590264f8f41feaaf935afbf527bb05451a543
js0ny <contact@js0ny.net>
2026-07-28 11:11:14 +0100

    load oil on demand

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -11,4 +11,27 @@ return {
     { '<leader>gg', '<Cmd>Neogit<CR>', desc = 'Neogit' },
     { '<C-S-g>', '<Cmd>Neogit<CR>', desc = 'Neogit' },
   },
+  opts = {
+    console_timeout = 0,
+    status = {
+      mode_text = {
+        M = '[M] modified',
+        N = '[N] new file',
+        A = '[A] added',
+        D = '[D] deleted',
+        C = '[C] copied',
+        U = '[U] updated',
+        R = '[R] renamed',
+        T = '[T] changed',
+        DD = '[DD] unmerged',
+        AU = '[AU] unmerged',
+        UD = '[UD] unmerged',
+        UA = '[UA] unmerged',
+        DU = '[DU] unmerged',
+        AA = '[AA] unmerged',
+        UU = '[UU] unmerged',
+        ['?'] = '',
+      },
+    },
+  },
 }
--- lua/plugins/tools/oil.lua
+++ lua/plugins/tools/oil.lua
@@ -1,11 +1,12 @@
 return {
-  -- 'stevearc/oil.nvim',
-  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
-  -- lazy = false,
-  -- ---@module 'oil'
-  -- ---@type oil.SetupOpts
-  -- opts = {
-  --   delete_to_trash = true,
-  --   default_file_explorer = true,
-  -- },
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = true,
+  cmd = { 'Oil' }, -- only load when explicitly call `:Oil`
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    delete_to_trash = true,
+    default_file_explorer = true,
+  },
 }
