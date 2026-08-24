625ba35fc6fdfcba66389bb60873ddf5a94229d6
js0ny <contact@js0ny.net>
2026-08-24 15:48:41 +0800

    feat: schemastore, merge json and jsonc

--- config/lang/json.nix
+++ config/lang/json.nix
@@ -1,16 +1,34 @@
-{ lib, ... }: {
+{ lib, ... }:
+let
+  inherit (lib.nixvim.utils) listToUnkeyedAttrs;
+  prettier = (
+    /*nixfmt:disable*/
+    listToUnkeyedAttrs [ "prettierd" "prettier" ]
+    // { stop_after_first = true; }
+    /*nixfmt:enable*/
+  );
+  localOpts = {
+    expandtab = true;
+    tabstop = 2;
+    softtabstop = 2;
+    shiftwidth = 2;
+  };
+in
+{
   plugins = {
-    conform-nvim.settings.formatters_by_ft.json = [ "jq" ];
+    conform-nvim.settings.formatters_by_ft = {
+      json = [ "jq" ];
+      jsonc = prettier;
+    };
 
     lsp.servers.jsonls.enable = lib.mkDefault false;
-  };
-
-  files."after/ftplugin/json.lua" = {
-    localOpts = {
-      expandtab = true;
-      tabstop = 2;
-      softtabstop = 2;
-      shiftwidth = 2;
+    schemastore = {
+      enable = true;
+      json.enable = true;
     };
+  };
+  files = {
+    "after/ftplugin/json.lua" = { inherit localOpts; };
+    "after/ftplugin/jsonc.lua" = { inherit localOpts; };
   };
 }
